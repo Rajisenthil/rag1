2eb54dc593ec72add4369327388837b83bf46d18
Rajisenthil <rajiammudr@gmail.com>
2017-04-21 18:57:37 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,17 @@
+# rag1
+#include<iostream.h>
+int main()
+{
+int a;
+cout<<"Enter number"<<endl;
+cin>>a;
+if(a>=0)
+{
+cout<<"Positive number";
+}
+else
+{
+cout<<"Negative number";
+}
+return 0;
+}
